8e6d51d84cb985b5dfda2ffa27da89f698660840
Om <poonjaniom11@gmail.com>
2024-04-09 20:43:16 +0530

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
-	public String PORT = System.getenv("PORT");
+//	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
